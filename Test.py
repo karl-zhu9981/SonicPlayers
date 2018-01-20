4b1e629b565b981c77fb08b8339bf978308e9abd
Alex Latunski <latunska@gmail.com>
2018-01-20 02:19:54 -0500

    Some cleaning up of the Final Code

--- Test.py
+++ Test.py
@@ -2,15 +2,32 @@ import pyautogui as press
 import pyaudio
 import wave
 import sys
+from matplotlib.mlab import find
+import numpy as np
+import math
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 CHUNK = 512
-RECORD_SECONDS = 1/60
+RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "file.wav"
 
 audio = pyaudio.PyAudio()
+
+
+#Turns raw audio input into frequency data
+def Pitch(signal):
+    signal = np.fromstring(signal, 'Int16');
+    crossing = [math.copysign(1.0, s) for s in signal]
+    index = find(np.diff(crossing));
+    f0=round(len(index) *RATE /(2*np.prod(len(signal))))
+    return f0;
+
+def noteFinder(freq):
+    if(freq == 690):
+        return "B"
+
 
 # start Recording
 stream = audio.open(format=FORMAT, channels=CHANNELS,
@@ -19,13 +36,16 @@ stream = audio.open(format=FORMAT, channels=CHANNELS,
 print("recording...")
 
 frames = []
-
+frequencies = []
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
 # Takes data from stream over duration of 1024 frames(Chunk Size)
     data = stream.read(CHUNK)
-# Adds Data to
+# Adds Data to frame for save to .wav file
     frames.append(data)
-    print(i)
+# Adds Data to frame for save to Frequency file
+    Frequency=Pitch(data)
+    
+    frequencies.append(Frequency)
 print("finished recording")
 
 
