1d426c472ac5cd221226d719b38f14c89844f004
airsick <colerbird@gmail.com>
2018-01-20 02:32:03 -0500

    This one works cause I'm da best

---
+++ soundtest Airsick.py
@@ -0,0 +1,43 @@
+from matplotlib.mlab import find
+import pyaudio
+import numpy as np
+import math
+
+
+chunk = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 6
+
+
+def Pitch(signal):
+    signal = np.fromstring(signal, 'Int16');
+    crossing = [math.copysign(1.0, s) for s in signal]
+    index = find(np.diff(crossing));
+    f0=round(len(index) *RATE /(2*np.prod(len(signal))))
+    return f0;
+
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format = FORMAT,
+channels = CHANNELS,
+rate = RATE,
+input = True,
+output = True,
+frames_per_buffer = chunk)
+
+for i in range(0, int(RATE / chunk * RECORD_SECONDS)):
+    data = stream.read(chunk)
+    Frequency=Pitch(data)
+    if(Frequency>2900 and Frequency<3000):
+    	print("b")
+    if(Frequency == 2627 or Frequency == 2606 or Frequency == 2649 or Frequency == 2584):
+    	print("a")
+    if(Frequency == 2326 or Frequency == 2304 or Frequency == 2347 or Frequency == 2369):
+    	print("g")
+    elif(Frequency==581 or Frequency == 560 or Frequency == 603):
+    	print("Low D")
+    else:
+    	print ("Frequency: ",Frequency)
